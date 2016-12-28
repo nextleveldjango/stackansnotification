16003dcc69952ef7922cf8c8cd591bf647eaf522
Prashant Gaur <IMFCORP\prashant.gaur@noiclt350.imfcorp.com>
2016-12-28 15:05:58 +0530

    Code cleaning

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 stackansnotification
 ====================
 
-it will give me notification on python console whether i have received answers of any question asked at stackoverflow.
+This script will check stackoverflow.com for given Question Url and update whether answer has been given or not.I wrote this script because i was suppose to check stackoverflow website in every few minutes to check whether somebody answered or not for a question i asked. My current Email was not registered with my stackoverflow account too.
+
+Required :  bs4  (BeautifulSoup)
+
+Please edit send_email function to get email.
--- notifier.py
+++ notifier.py
@@ -1,26 +1,49 @@
+import sys
+import time
 import urllib2
-import smtplib
 
 from bs4 import BeautifulSoup
 
-def func(send_email):
-    url = urllib2.urlopen("http://stackoverflow.com/questions/14050824/add-sum-of-values-of-two-lists-into-new-list")
-    data = url.read()
-    soup = BeautifulSoup(data)
-    if soup.find('div', class_="answers-subheader").h2.__str__() != "'<h2>\n</h2>":
-        message = "Congrats , You got a answer."
-        try:
-            smtpObj = smtplib.SMTP('127.0.0.1', 25, 'localhost')
-            smtpObj = smtplib.SMTP('localhost')
-            smtpObj.sendmail(sender, receivers, message)        
-            print "Successfully sent email"
-        except:
-            print "Error: unable to send email"
-        return "Done"
-    print "Still waiting for answer."
-    return "No"
+
+def send_email():
+    print('SENDING EMAIL')
+
+
+def main(email_sent, url):
+    """
+    """
+    try:
+        url = urllib2.urlopen(url)
+        data = url.read()
+
+        soup = BeautifulSoup(data)
+        if(not soup):
+            raise Exception
+
+        answer_div = soup.find('div', class_="answers-subheader")
+        if(not answer_div):
+            raise Exception
+        assert answer_div.h2.__str__() != "<h2>\n</h2>"
+        send_email()
+        print("Got Answer, :) :-> Email Sent")
+        return True
+    except AssertionError:
+        print('No Answer') 
+        time.sleep(5)
+        return False
+    except Exception as exc:
+        print('Problem with Url, please pass valid stackoverflow url', exc.message)
+        return True
+
 
 if __name__ == '__main__':
-    send_email = "No"
-    while send_email == "No":
-        send_email = func(send_email) 
+    """
+    """
+    try:
+        url = sys.argv[1]
+    except KeyError:
+        print('Please pass valid stackoverflow url')
+        sys.exit()
+    email_sent = False
+    while(not email_sent):
+        email_sent = main(email_sent, url) 
